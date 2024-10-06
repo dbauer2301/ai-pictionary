ec0b29046cb15296e069b17f9341656eb5c021d8
BDENNIS <dennis.bauer@occam37.com>
2024-10-06 15:01:27 +0200

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        'brand-1': 'var(--brand-1)',
+        'brand-2': 'var(--brand-2)',
+        'brand-1-dark': 'var(--brand-1-dark)',
+        'brand-2-dark': 'var(--brand-2-dark)',
+        white: 'var(--white)',
+        black: 'var(--black)',
+        'primary-container': 'var(--primary-container)',
+        'text-primary': 'var(--text-primary)',
+        'text-inverted': 'var(--text-inverted)',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        'titillium-web': ['"Titillium Web"', 'sans-serif'],
       },
     },
   },
